40d1a30a1f13acf1e25774d916f77355735203d5
Roni Saha <roni.cse@gmail.com>
2016-12-20 17:47:48 +0600

    repeater form control

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {FormGroup, FormControl, Validators, FormArray} from "@angular/forms";
 
 @Component({
   selector: 'app-user',
@@ -15,10 +15,26 @@ export class UserComponent implements OnInit {
     this.userForm = new FormGroup({
       'username': new FormControl('', [Validators.required]),
       'email': new FormControl('', [Validators.required, this.validateEmail]),
+      'contactNumbers': new FormArray([UserComponent.getPhoneNumberFormGroup()]),
     });
 
     this.userForm.addControl('password', new FormControl('', [Validators.required]));
     this.userForm.addControl('confirmPassword', new FormControl('', [Validators.required, this.passwordMatch.bind(this)]));
+  }
+
+  addContactNumber() {
+    this.userForm.get("contactNumbers").push(UserComponent.getPhoneNumberFormGroup());
+  }
+
+  private static getPhoneNumberFormGroup() {
+    return new FormGroup({
+      'number': new FormControl('', [Validators.required]),
+      'type': new FormControl('', [Validators.required]),
+    });
+  }
+
+  removePhoneNumber(i) {
+    this.userForm.get("contactNumbers").removeAt(i);
   }
 
   onSubmit(){
@@ -26,7 +42,7 @@ export class UserComponent implements OnInit {
   }
 
   validateEmail(c:FormControl) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!re.test(c.value)) {
       return {email: 'Invalid Email'};
     }
